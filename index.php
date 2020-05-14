8ea195b959a9848d51a3b86c04381d278d96ac67
Elric Barkey <ebarkey2@mail.greenriver.edu>
2020-05-13 20:49:52 -0400

    added session variables for each piece of info for each page

--- index.php
+++ index.php
@@ -1,21 +1,117 @@
 <?php
-
 //turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//require the autoload file
-require_once ('vendor/autoload.php');
+//start a session
+session_start();
 
-//create an instance of the base class
+//require the auto load file
+require_once ("vendor/autoload.php");
+require_once ("model/data-layer.php");
+//require_once ("model/validate.php");
+
+//instantiate the F3 Base class
 $f3 = Base::instance();
 
-//define a default route
-$f3->route('GET /', function(){
-
+//default route
+$f3->route('GET /', function()
+{
     $view = new Template();
     echo $view->render('views/home.html');
 });
 
-//run fat free
+//personal Information route
+$f3->route('GET|POST /personal', function($f3)
+{
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        //var_dump($_POST);
+
+        //store the data in the session array
+        $_SESSION['fname'] = $_POST['fname'];
+        $_SESSION['lname'] = $_POST['lname'];
+        $_SESSION['phone'] = $_POST['phone'];
+        $_SESSION['gender'] = $_POST['gender'];
+        $_SESSION['age'] = $_POST['age'];
+
+
+        $f3->reroute('profile');
+        session_destroy();
+    }
+    /*
+    $f3->set('myfname', $_SESSION['fname']);
+    $f3->set('lname', $_SESSION['lname']);
+    $f3->set('phone', $_SESSION['seeking']);
+    $f3->set('gender', $_SESSION['gender']);
+    $f3->set('age', $_SESSION['age']);
+    */
+
+    $view = new Template();
+    echo $view->render('views/personalInfo.html');
+});
+
+//profile
+$f3->route('GET|POST /profile', function($f3)
+{
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //var_dump($_POST);
+
+        //store the data in the session array
+        $_SESSION['email'] = $_POST['email'];
+        $_SESSION['state'] = $_POST['state'];
+        $_SESSION['seeking'] = $_POST['seeking'];
+        $_SESSION['bio'] = $_POST['bio'];
+
+
+        $f3->reroute('interests');
+        session_destroy();
+    }
+
+    /*
+    $f3->set('email', $_SESSION['email']);
+    $f3->set('state', $_SESSION['state']);
+    $f3->set('seeking', $_SESSION['seeking']);
+    $f3->set('bio', $_SESSION['bio']);
+    */
+
+    $view = new Template();
+    echo $view->render('views/profile.html');
+});
+
+//interests
+$f3->route('GET|POST /interests', function($f3)
+{
+    $indoor = getIndoor();
+    $outdoor = getOutdoor();
+
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //var_dump($_POST);
+
+        //store the data in the session array
+        $_SESSION['indoor'] = $_POST['indoor'];
+        $_SESSION['outdoor'] = $_POST['outdoor'];
+
+
+        $f3->reroute('summary');
+        session_destroy();
+    }
+    $f3->set('interests', $indoor);
+    $f3->set('interests2', $outdoor);
+
+    $view = new Template();
+    echo $view->render('views/interests.html');
+});
+
+//summary
+$f3->route('GET /summary', function()
+{
+    var_dump($_SESSION);
+    $view = new Template();
+    echo $view->render('views/summary.html');
+});
+
+//run f3
 $f3->run();
+
